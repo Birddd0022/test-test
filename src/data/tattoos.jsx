b2975148da8ee1aa0800e47db6a3ef0e95e1c6d8
Birddd0022 <162594221+Birddd0022@users.noreply.github.com>
2024-04-16 19:07:41 +0700

    Add files via upload

---
+++ src/data/tattoos.jsx
@@ -0,0 +1,34 @@
+const tattoos = [
+    {
+        title: "แขนเท่ๆ",
+        thumbnailUrl: "/images/tattoo-01-small.jpg",
+        fullUrl: "/images/tattoo-01-large.jpg"
+    },
+    {
+        title: "มือเท่ๆ",
+        thumbnailUrl: "/images/tattoo-02-small.jpg",
+        fullUrl: "/images/tattoo-02-large.jpg"
+    },
+    {
+        title: "คอเท่ๆ",
+        thumbnailUrl: "/images/tattoo-03-small.jpg",
+        fullUrl: "/images/tattoo-03-large.jpg"
+    },
+    {
+        title: "หลังเท่ๆ",
+        thumbnailUrl: "/images/tattoo-04-small.jpg",
+        fullUrl: "/images/tattoo-04-large.jpg"
+    },
+    {
+        title: "แขนเท่ๆ",
+        thumbnailUrl: "/images/tattoo-05-small.jpg",
+        fullUrl: "/images/tattoo-05-large.jpg"
+    },
+    {
+        title: "แขนเท่ๆ",
+        thumbnailUrl: "/images/tattoo-06-small.jpg",
+        fullUrl: "/images/tattoo-06-large.jpg"
+    }
+];
+
+export default tattoos;
